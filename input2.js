0e367fdae017c12078ec3034d0a08fd60e01e963
pwj2 <pwj2@fresnou.org>
2023-02-16 14:16:39 -0800

    Added Input1.js

---
+++ input2.js
@@ -0,0 +1,12 @@
+const readline = require('readline-sync');
+
+//1
+let num1 = Number(readline.question( "Enter number"));
+let num2 = Number(readline.question("Enter 2nd number:" ));
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+
+//2
+let num3 = Number(readline.question("Enter number"));
+let num4 = Number(readline.question("Enter 2nd number:" ));
+console.log(`${num3} - ${num4} = ${num3 - num4}`);
+
