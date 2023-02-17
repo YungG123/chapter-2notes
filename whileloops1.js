d76536f6b4c9559bfb6fd9eae442d5790fa1bfe5
pwj2 <pwj2@fresnou.org>
2023-02-17 14:14:28 -0800

    Added Input1.js

---
+++ whileloops1.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync');
+
+// let y = 0;
+// while (y < 25){
+//     y++;
+//     console.log(y)
+// }
+
+// let y =  0;
+// while(y <50){
+//     y++;
+//     console.log(y);
+//}
+
+// let y = -10
+// while (y <10){
+//     y++;
+//     console.log(y);
+// }
+
+let y = 1;
+while(y > -50){
+    y--;
+    console.log(y)
+}
